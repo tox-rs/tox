6e62980f31bcb3ec3357cec51d14c17df17763d0
Zetok Zalbavar <zexavexxe@gmail.com>
2016-07-08 15:25:50 +0100

    chore(travis): add more stuff necessary for coveralls
    Apparently `travis-cargo` script doesn't like to reliably upload results
    to coveralls:
    https://github.com/huonw/travis-cargo/issues/58
    
    Also don't change working directory when deploying docs.

--- scripts/deploy-gh-pages.sh
+++ scripts/deploy-gh-pages.sh
@@ -2,7 +2,7 @@
 
 # used in travis to deploy documentation to gh-pages branch
 
-
+(
 cd target/doc
 git init
 git config user.name "Travis CI"
@@ -11,3 +11,4 @@ echo "<meta http-equiv=refresh content=0;url=tox/index.html>" > index.html
 git add .
 git commit --quiet -m "Deploy to GH pages."
 git push --force --quiet "https://${GH_TOKEN}@github.com/zetok/tox.git" master:gh-pages &> /dev/null
+)
