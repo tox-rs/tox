b181da089b5f286e3309f8aa95454b456c4506de
Roman Proskuryakov <humbug@deeptown.org>
2017-11-22 01:25:03 +0300

    feat(example): move client handshake code into function

--- examples/tcp_client.rs
+++ examples/tcp_client.rs
@@ -37,34 +37,14 @@ use tokio_io::*;
 use tokio_core::reactor::Core;
 use tokio_core::net::TcpStream;
 
-fn main() {
-    // Server constant PK for examples/tests
-    // Use `gen_keypair` to generate random keys
-    let server_pk = PublicKey([177, 185, 54, 250, 10, 168, 174,
-                            148, 0, 93, 99, 13, 131, 131, 239,
-                            193, 129, 141, 80, 158, 50, 133, 100,
-                            182, 179, 183, 234, 116, 142, 102, 53, 38]);
-    // Some constant keypair for client
-    let client_pk = PublicKey([252, 72, 40, 127, 213, 13, 0, 95,
-                              13, 230, 176, 49, 69, 252, 220, 132,
-                              48, 73, 227, 58, 218, 154, 215, 245,
-                              23, 189, 223, 216, 153, 237, 130, 88]);
-    let client_sk = SecretKey([157, 128, 29, 197, 1, 72, 47, 56,
-                              65, 81, 191, 67, 220, 225, 108, 193,
-                              46, 163, 145, 242, 139, 125, 159,
-                              137, 174, 14, 225, 7, 138, 120, 185, 153]);
-
-    let addr = "0.0.0.0:12345".parse().unwrap();
-    let mut core = Core::new().unwrap();
-    let handle = core.handle();
-    let tcp = TcpStream::connect(&addr, &handle);
-    let process_handshake = tcp
-        .and_then(|socket| {
-            // create handshake
-            futures::done(create_client_handshake(client_pk, client_sk, server_pk))
-                .map(|(session, common_key, handshake)| (socket, session, common_key, handshake))
-        })
-        .and_then(|(socket, session, common_key, handshake)| {
+fn make_client_handshake(socket: TcpStream,
+    client_pk: PublicKey,
+    client_sk: SecretKey,
+    server_pk: PublicKey
+) -> Box<Future<Item=(TcpStream, secure::Channel), Error=Error>>
+{
+    let res = futures::done(create_client_handshake(client_pk, client_sk, server_pk))
+        .and_then(|(session, common_key, handshake)| {
             // send handshake
             socket.framed(handshake::ClientCodec)
                 .send(handshake)
@@ -74,7 +54,7 @@ fn main() {
                         format!("Could not send handshake::Client {:?}", e),
                     )
                 })
-                .map(move |socket| {
+                .map(|socket| {
                     (socket.into_inner(), session, common_key)
                 })
         })
@@ -103,14 +83,40 @@ fn main() {
                     (socket, channel)
                 })
         });
+    Box::new(res)
+}
 
-    let client = process_handshake.and_then(|(socket, channel)| {
-        println!("Handshake complited");
-        let secure_socket = socket.framed(codec::Codec::new(channel));
-        let (_to_server, _from_server) = secure_socket.split();
-        // use example https://github.com/jgallagher/tokio-chat-example/blob/master/tokio-chat-client/src/main.rs
-        Ok(())
-    });
+fn main() {
+    // Server constant PK for examples/tests
+    // Use `gen_keypair` to generate random keys
+    let server_pk = PublicKey([177, 185, 54, 250, 10, 168, 174,
+                            148, 0, 93, 99, 13, 131, 131, 239,
+                            193, 129, 141, 80, 158, 50, 133, 100,
+                            182, 179, 183, 234, 116, 142, 102, 53, 38]);
+    // Some constant keypair for client
+    let client_pk = PublicKey([252, 72, 40, 127, 213, 13, 0, 95,
+                              13, 230, 176, 49, 69, 252, 220, 132,
+                              48, 73, 227, 58, 218, 154, 215, 245,
+                              23, 189, 223, 216, 153, 237, 130, 88]);
+    let client_sk = SecretKey([157, 128, 29, 197, 1, 72, 47, 56,
+                              65, 81, 191, 67, 220, 225, 108, 193,
+                              46, 163, 145, 242, 139, 125, 159,
+                              137, 174, 14, 225, 7, 138, 120, 185, 153]);
+
+    let addr = "0.0.0.0:12345".parse().unwrap();
+    let mut core = Core::new().unwrap();
+    let handle = core.handle();
+    let client = TcpStream::connect(&addr, &handle)
+        .and_then(|socket| {
+            make_client_handshake(socket, client_pk, client_sk, server_pk)
+        })
+        .and_then(|(socket, channel)| {
+            println!("Handshake complited");
+            let secure_socket = socket.framed(codec::Codec::new(channel));
+            let (_to_server, _from_server) = secure_socket.split();
+            // use example https://github.com/jgallagher/tokio-chat-example/blob/master/tokio-chat-client/src/main.rs
+            Ok(())
+        });
 
     core.run(client).unwrap();
 }
