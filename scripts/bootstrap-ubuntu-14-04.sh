8166260097b07a412107b30eaea5b884ae37881c
Roman Proskuryakov <humbug@deeptown.org>
2017-12-27 05:39:38 +0300

    chore(sodium): update sodium deps

--- scripts/bootstrap-ubuntu-14-04.sh
+++ scripts/bootstrap-ubuntu-14-04.sh
@@ -2,7 +2,7 @@
 
 # used in travis to download & install libsodium
 
-wget http://archive.ubuntu.com/ubuntu/pool/universe/libs/libsodium/libsodium18_1.0.8-5_amd64.deb
-wget http://archive.ubuntu.com/ubuntu/pool/universe/libs/libsodium/libsodium-dev_1.0.8-5_amd64.deb
+wget http://archive.ubuntu.com/ubuntu/pool/universe/libs/libsodium/libsodium18_1.0.13-1_amd64.deb
+wget http://archive.ubuntu.com/ubuntu/pool/universe/libs/libsodium/libsodium-dev_1.0.13-1_amd64.deb
 
-sudo dpkg -i libsodium-dev_1.0.8-5_amd64.deb libsodium18_1.0.8-5_amd64.deb
+sudo dpkg -i libsodium-dev_1.0.13-1_amd64.deb libsodium18_1.0.13-1_amd64.deb
