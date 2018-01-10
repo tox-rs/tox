aa033d3986768dd3cb498550919569561c13beaf
Roman Proskuryakov <humbug@deeptown.org>
2018-01-10 04:42:09 +0300

    feat(example): add logging + send timeouts to tcp relay server example

--- examples/tcp_server.rs
+++ examples/tcp_server.rs
@@ -21,25 +21,32 @@
 
 extern crate tox;
 extern crate futures;
-extern crate bytes;
-extern crate nom;
 extern crate tokio_core;
 extern crate tokio_io;
+extern crate tokio_timer;
+
+#[macro_use]
+extern crate log;
+extern crate env_logger;
 
 use tox::toxcore::crypto_core::*;
 use tox::toxcore::tcp::make_server_handshake;
 use tox::toxcore::tcp::codec;
 use tox::toxcore::tcp::server::{Server, Client};
 
-use futures::{Sink, Stream, Future};
+use futures::prelude::*;
 use futures::sync::mpsc;
 
-use tokio_io::*;
+use tokio_io::AsyncRead;
 use tokio_core::reactor::Core;
 use tokio_core::net::TcpListener;
 
+use std::time;
+
 fn main() {
-    // Some constant keypair
+    env_logger::init().unwrap();
+    // Server constant PK for examples/tests
+    // Use `gen_keypair` to generate random keys
     let server_pk = PublicKey([177, 185, 54, 250, 10, 168, 174,
                             148, 0, 93, 99, 13, 131, 131, 239,
                             193, 129, 141, 80, 158, 50, 133, 100,
@@ -53,67 +60,74 @@ fn main() {
     let handle = core.handle();
     let listener = TcpListener::bind(&addr, &handle).unwrap();
 
-    println!("Listening on {} using PK {:?}", addr, &server_pk.0);
+    info!("Listening on addr={}, {:?}", addr, &server_pk);
 
     let server_inner = Server::new();
 
     // TODO move this processing future into a standalone library function
     let server = listener.incoming().for_each(|(socket, addr)| {
-        println!("A new client connected from {}", addr);
+        debug!("A new client connected from {}", addr);
 
         let server_inner_c = server_inner.clone();
         let register_client = make_server_handshake(socket, server_sk.clone())
             .map_err(|e| {
-                println!("handshake error: {}", e);
+                error!("handshake error: {}", e);
                 e
             })
             .and_then(move |(socket, channel, client_pk)| {
-                println!("Handshake for client {:?} complited", &client_pk);
+                debug!("Handshake for client {:?} complited", &client_pk);
                 let (tx, rx) = mpsc::unbounded();
                 server_inner_c.insert(Client::new(tx, &client_pk));
 
                 Ok((socket, channel, client_pk, rx))
             });
+
         let server_inner_c = server_inner.clone();
         let process_connection = register_client
             .and_then(move |(socket, channel, client_pk, rx)| {
+                let server_inner_c_c = server_inner_c.clone();
                 let secure_socket = socket.framed(codec::Codec::new(channel));
                 let (to_client, from_client) = secure_socket.split();
 
                 // reader = for each Packet from client process it
-                let server_inner_c_c = server_inner_c.clone();
                 let reader = from_client.for_each(move |packet| {
-                    println!("Handle {:?} => {:?}", client_pk, packet);
-                    server_inner_c_c.handle_packet(&client_pk, packet)
+                    debug!("Handle {:?} => {:?}", client_pk, packet);
+                    server_inner_c.handle_packet(&client_pk, packet)
                 });
 
+                let writer_timer = tokio_timer::wheel()
+                    .tick_duration(time::Duration::from_secs(1))
+                    .build()
+                ;
                 // writer = for each Packet from rx send it to client
                 let writer = rx
                     .map_err(|()| unreachable!("rx can't fail"))
                     .fold(to_client, move |to_client, packet| {
-                        println!("Send {:?} => {:?}", client_pk, packet);
-                        to_client.send(packet)
+                        debug!("Send {:?} => {:?}", client_pk, packet);
+                        let sending_future = to_client.send(packet);
+                        let duration = time::Duration::from_secs(30);
+                        let timeout = writer_timer.timeout(sending_future, duration);
+                        timeout
                     })
                     // drop to_client when rx stream is exhausted
                     .map(|_to_client| ());
 
                 // TODO ping request = each 30s send PingRequest to client
 
-                let server_inner_c_c = server_inner_c.clone();
                 reader.select(writer)
                     .map(|_| ())
                     .map_err(move |(err, _select_next)| {
-                        println!("Processing client {:?} ended with error: {:?}", &client_pk, err);
+                        error!("Processing client {:?} ended with error: {:?}", &client_pk, err);
                         err
                     })
                     .then(move |r_processing| {
-                        println!("shutdown PK {:?}", &client_pk);
+                        debug!("shutdown PK {:?}", &client_pk);
                         server_inner_c_c.shutdown_client(&client_pk)
                             .then(move |r_shutdown| r_processing.and(r_shutdown))
                     })
             });
         handle.spawn(process_connection.then(|r| {
-            println!("end of processing with result {:?}", r);
+            debug!("end of processing with result {:?}", r);
             Ok(())
         }));
 
