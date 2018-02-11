217252b573775a86073796ee00daeccf2456e7cd
Namsoo CHO <nscho66@gmail.com>
2018-02-11 13:03:54 +0900

    refactor(dht): receive channel for DhtNode object

--- examples/dht_node_new.rs
+++ examples/dht_node_new.rs
@@ -28,10 +28,12 @@ extern crate bytes;
 extern crate log;
 
 use futures::*;
+use futures::sync::mpsc;
 use tokio_core::reactor::{Core, Handle};
-use tokio_core::net::UdpSocket;
+use tokio_core::net::{UdpSocket};
 
 use std::cell::RefCell;
+use std::collections::HashMap;
 use std::net::SocketAddr;
 use std::rc::Rc;
 use std::io::{self};
@@ -40,26 +42,46 @@ use tox::toxcore::dht_new::codec::*;
 use tox::toxcore::dht_new::packet::*;
 use tox::toxcore::dht_new::dht_node::*;
 
+/// Shorthand for the transmit half of the message channel.
+type Tx = mpsc::UnboundedSender<DhtBase>;
+
+/// Shorthand for the receive half of the message channel.
+type Rx = mpsc::UnboundedReceiver<DhtBase>;
+
 struct Shared {
-    // udp listening socket
+    peers: HashMap<SocketAddr, Tx>,
     listener: SocketAddr,
-    // tokio core handle
     handle: Handle,
 }
 
 /// The state for each connected client.
 struct Peer {
-    // shared data for operating server.
     state: Rc<RefCell<Shared>>,
-    // peer socket address
+
+    /// Receive half of the message channel.
+    ///
+    /// This is used to receive messages from DhtNode object. When a message is received
+    /// off of this `Rx`, it will be written to the socket.
+    rx: Rx,
+
+    /// Client socket address.
+    ///
+    /// The socket address is used as the key in the `peers` HashMap. The
+    /// address is saved so that the `Peer` drop implementation can clean up its
+    /// entry.
     addr: SocketAddr,
-    // DhtBase packet to send, If DhtNode need to send packet, this field is used.
+
+    /// DhtBase packet to send
     to_send: Result<Option<DhtBase>, io::Error>,
+
+    /// buffer to receive DhtBase packet from rx
+    buf: Vec<DhtBase>,
 }
 
 impl Shared {
     fn new(listener: SocketAddr, handle: Handle) -> Self {
         Shared {
+            peers: HashMap::new(),
             listener: listener,
             handle: handle,
         }
@@ -71,10 +93,18 @@ impl Peer {
             state: Rc<RefCell<Shared>>,
             packet: Result<Option<DhtBase>, io::Error>) -> Peer
     {
+        // Create a channel for this peer
+        let (tx, rx) = mpsc::unbounded();
+
+        // Add an entry for this `Peer` in the shared state map.
+        state.borrow_mut().peers.insert(addr, tx);
+
         Peer {
             state: state,
+            rx: rx,
             addr: addr,
             to_send: packet,
+            buf: Vec::new(),
         }
     }
 }
@@ -94,7 +124,38 @@ impl Future for Peer {
 
         self.to_send = Ok(None);
 
+        // Receive all packets from DhtNode object.
+        loop {
+            // Polling an `UnboundedReceiver` cannot fail, so `unwrap` here is
+            // safe.
+            if let Async::Ready(Some(v)) = self.rx.poll().unwrap() {
+                self.buf.push(v);
+            } else {
+                break;
+            }
+        }
+
+        // Flush the write buffer to the socket
+        self.buf.iter()
+            .map(|packet| {
+                let ref peer = self.state.borrow();
+                let listener = UdpSocket::bind(&peer.listener, &peer.handle).unwrap();
+                let (sink, _) = listener.framed(DhtCodec).split();;
+                let to_send = sink.send((self.addr, packet.clone()));
+                &peer.handle.spawn(to_send.then(|_| Ok(())));
+            })
+            .collect::<Vec<_>>();
+
+        self.buf.clear();
+
         Ok(Async::NotReady)
+    }
+}
+
+impl Drop for Peer {
+    fn drop(&mut self) {
+        self.state.borrow_mut().peers
+            .remove(&self.addr);
     }
 }
 
@@ -110,6 +171,11 @@ pub fn main() {
 
     let node = Rc::new(RefCell::new(DhtNode::new().unwrap()));
     let (_, stream) = listener.framed(DhtCodec).split();
+    // Create the shared state. This is how all the peers communicate.
+    //
+    // The server task will hold a handle to this. For every new client, the
+    // `state` handle is cloned and passed into the task that processes the
+    // client connection.
     let state = Rc::new(RefCell::new(Shared::new(addr, handle.clone())));
     // The server task asynchronously iterates over and processes each
     // incoming packet.
@@ -123,6 +189,9 @@ pub fn main() {
         Ok(())
     })
     .map_err(|err| {
+        // All tasks must have an `Error` type of `()`. This forces error
+        // handling and helps avoid silencing failures.
+        //
         error!("packet receive error = {:?}", err);
     });
 
