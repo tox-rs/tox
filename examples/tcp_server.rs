f7d9cc9ccf5517c83f1fe9b4f32589ff90612b4f
Evgeny Kurnevsky <kurnevsky@gmail.com>
2018-07-05 13:07:50 +0300

    refactor(tcp_server): move TCP server processing to core

--- examples/tcp_server.rs
+++ examples/tcp_server.rs
@@ -8,18 +8,11 @@ extern crate log;
 extern crate env_logger;
 
 use tox::toxcore::crypto_core::*;
-use tox::toxcore::tcp::handshake::make_server_handshake;
-use tox::toxcore::tcp::codec;
-use tox::toxcore::tcp::server::{Server, ServerProcessor};
+use tox::toxcore::tcp::server::{Server, ServerExt};
 
 use futures::prelude::*;
 
-use tokio::util::FutureExt;
 use tokio::net::TcpListener;
-use tokio_codec::Framed;
-
-use std::time;
-use std::io::{Error, ErrorKind};
 
 fn main() {
     env_logger::init();
@@ -38,95 +31,18 @@ fn main() {
 
     info!("Listening on addr={}, {:?}", addr, &server_pk);
 
-    // Ignore all TCP onion requests for now
-    let server_inner = Server::new();
+    let server = Server::new();
 
-    // TODO move this processing future into a standalone library function
-    let server = listener.incoming().for_each(move |socket| {
-        let addr = socket.peer_addr()
-            .map_err(|e| {
-                error!("could not get peer addr: {}", e);
-                e
-            })?;
-        debug!("A new client connected from {}", addr);
-
-        let register_client = make_server_handshake(socket, server_sk.clone())
-            .map_err(|e|
-                Error::new(ErrorKind::Other,
-                    format!("Handshake error {:?}", e))
-            )
-            .map(|(socket, channel, client_pk)| {
-                debug!("Handshake for client {:?} complited", &client_pk);
-                (socket, channel, client_pk)
-            });
-
-        let server_inner_c = server_inner.clone();
-        let process = register_client.and_then(move |(socket, channel, client_pk)| {
-            let secure_socket = Framed::new(socket, codec::Codec::new(channel));
-            let (to_client, from_client) = secure_socket.split();
-            let ServerProcessor { from_client_tx, to_client_rx, processor } =
-                ServerProcessor::create(
-                    server_inner_c,
-                    client_pk.clone(),
-                    addr.ip(),
-                    addr.port()
-                );
-
-            // writer = for each Packet from to_client_rx send it to client
-            let writer = to_client_rx
-                .map_err(|()| Error::from(ErrorKind::UnexpectedEof))
-                .fold(to_client, move |to_client, packet| {
-                    debug!("Send {:?} => {:?}", client_pk, packet);
-                    to_client.send(packet)
-                        .deadline(time::Instant::now() + time::Duration::from_secs(30))
-                        .map_err(|_|
-                            Error::new(ErrorKind::Other,
-                                format!("Writer timed out"))
-                        )
-                })
-                // drop to_client when to_client_rx stream is exhausted
-                .map(|_to_client| ())
-                .map_err(|_|
-                    Error::new(ErrorKind::Other,
-                        format!("Writer ended with error"))
-                );
-
-            // reader = for each Packet from client send it to server processor
-            let reader = from_client
-                .forward(from_client_tx
-                    .sink_map_err(|e|
-                        Error::new(ErrorKind::Other,
-                            format!("Could not forward message from client to server {:?}", e))
-                    )
-                )
-                .map(|(_from_client, _sink_err)| ())
-                .map_err(|_|
-                    Error::new(ErrorKind::Other,
-                        format!("Reader ended with error"))
-                );
-
-            processor
-                .select(reader)
-                    .map_err(move |(err, _select_next)| {
-                        err
-                    })
-                    .map(|_| ())
-                .select(writer)
-                    .map_err(move |(err, _select_next)| {
-                        err
-                    })
-        });
-
-        tokio::spawn( process.map(|_| ()).map_err(|_| ()) );
-
-        Ok(())
-    })
-    .map_err(|err| {
+    let future = listener.incoming()
+        .for_each(move |stream|
+            server.clone().run(stream, server_sk.clone())
+        )
+        .map_err(|err| {
             // All tasks must have an `Error` type of `()`. This forces error
             // handling and helps avoid silencing failures.
             //
             // In our example, we are only going to log the error to STDOUT.
-            println!("listener.incoming() error = {:?}", err);
-    });
-    tokio::run(server);
+            println!("Server error = {:?}", err);
+        });
+    tokio::run(future);
 }
