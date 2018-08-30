e9761313a2865f4ecaf04710fb0e92ff81097ef3
NamsooCho <nscho66@gmail.com>
2018-08-30 15:00:59 +0900

    feat(fr_conn): add parser of ShareRelays

--- src/toxcore/friend_connection/packet/mod.rs
+++ src/toxcore/friend_connection/packet/mod.rs
@@ -4,8 +4,10 @@
 use toxcore::binary_io::*;
 
 mod id_alive;
+mod share_relays;
 
 pub use self::id_alive::*;
+pub use self::share_relays::*;
 
 /** Friend connection packet enum that encapsulates all types of Friend connection packets.
 */
@@ -13,18 +15,22 @@ pub use self::id_alive::*;
 pub enum Packet {
     /// [`IdAlive`](./struct.IdAlive.html) structure.
     IdAlive(IdAlive),
+    /// [`ShareRelays`](./struct.ShareRelays.html) structure.
+    ShareRelays(ShareRelays),
 }
 
 impl ToBytes for Packet {
     fn to_bytes<'a>(&self, buf: (&'a mut [u8], usize)) -> Result<(&'a mut [u8], usize), GenError> {
         match *self {
             Packet::IdAlive(ref p) => p.to_bytes(buf),
+            Packet::ShareRelays(ref p) => p.to_bytes(buf),
         }
     }
 }
 
 impl FromBytes for Packet {
     named!(from_bytes<Packet>, alt!(
-        map!(IdAlive::from_bytes, Packet::IdAlive)
+        map!(IdAlive::from_bytes, Packet::IdAlive) |
+        map!(ShareRelays::from_bytes, Packet::ShareRelays)
     ));
 }
---
+++ src/toxcore/friend_connection/packet/share_relays.rs
@@ -0,0 +1,69 @@
+/*! ShareRalays struct
+*/
+
+use toxcore::binary_io::*;
+use toxcore::dht::packed_node::*;
+
+/** ShareRelays is a struct that holds at most 3 TCP relays as a PackedNode format.
+
+This packet is used to share relay info. between two friends.
+
+Serialized form:
+
+Length    | Content
+--------- | ------
+`1`       | `0x11`
+variable  | 3 PackedNodes
+
+*/
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct ShareRelays {
+    relays: Vec<PackedNode>,
+}
+
+impl FromBytes for ShareRelays {
+    named!(from_bytes<ShareRelays>, do_parse!(
+        tag!("\x11") >>
+        relays: many0!(PackedNode::from_tcp_bytes) >>
+        verify!(value!(relays.len()), |len| len <= 3) >>
+        (ShareRelays {
+            relays,
+        })
+    ));
+}
+
+impl ToBytes for ShareRelays {
+    fn to_bytes<'a>(&self, buf: (&'a mut [u8], usize)) -> Result<(&'a mut [u8], usize), GenError> {
+        do_gen!(buf,
+            gen_be_u8!(0x11) >>
+            gen_cond!(self.relays.len() > 3, |buf| gen_error(buf, 0)) >>
+            gen_many_ref!(&self.relays, |buf, relay| PackedNode::to_tcp_bytes(relay, buf))
+        )
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use toxcore::crypto_core::*;
+
+    encode_decode_test!(
+        share_relays_encode_decode,
+        ShareRelays {
+            relays: vec![
+                PackedNode {
+                    saddr: "1.1.1.1:33445".parse().unwrap(),
+                    pk: gen_keypair().0,
+                },
+                PackedNode {
+                    saddr: "1.1.1.1:33446".parse().unwrap(),
+                    pk: gen_keypair().0,
+                },
+                PackedNode {
+                    saddr: "1.1.1.1:33447".parse().unwrap(),
+                    pk: gen_keypair().0,
+                },
+            ],
+        }
+    );
+}
