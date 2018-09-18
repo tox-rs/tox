5820afbf18560b541196f2784bd25c32468c98c0
Evgeny Kurnevsky <kurnevsky@gmail.com>
2018-09-18 11:28:09 +0300

    refactor(crypto): get rid of quickcheck in crypto core tests

--- src/toxcore_tests/crypto_core_tests.rs
+++ src/toxcore_tests/crypto_core_tests.rs
@@ -1,12 +1,9 @@
 //! Tests for `crypto_core` module.
 
-use std::str::FromStr;
 use std::thread;
 
 use toxcore::binary_io::*;
 use toxcore::crypto_core::*;
-
-use quickcheck::quickcheck;
 
 
 // crypto_init()
@@ -15,26 +12,21 @@ use quickcheck::quickcheck;
 // test whether crypto init behaves well when one tries to run it in parallel.
 // Its code should **not** be ran in parallel.
 fn crypto_init_test() {
-    fn with_threads(num: u8) {
-        thread::spawn(move || {
-            for _ in 0..num {
-                thread::spawn(move || {
-                    assert_eq!(true, crypto_init());
-                    // second run, value should be the same
-                    assert_eq!(true, crypto_init());
-                });
-            }
-        });
-    }
-
     // apparently hundreds of threads can cause OOM on appveyor
     #[cfg(not(target_os = "windows"))]
-    {
-        quickcheck(with_threads as fn(u8));
-        with_threads(255);
-    }
+    let num = 255;
     #[cfg(target_os = "windows")]
-    with_threads(32);
+    let num = 32;
+
+    thread::spawn(move || {
+        for _ in 0..num {
+            thread::spawn(move || {
+                assert_eq!(true, crypto_init());
+                // second run, value should be the same
+                assert_eq!(true, crypto_init());
+            });
+        }
+    });
 }
 
 
@@ -98,24 +90,12 @@ fn random_usize_test() {
 #[test]
 fn public_key_valid_test() {
     let (pk, _) = gen_keypair();
-    assert_eq!(true, public_key_valid(&pk));
+    assert!(public_key_valid(&pk));
 
-    assert_eq!(true, public_key_valid(&PublicKey([0; PUBLICKEYBYTES]))); // 0
-    assert_eq!(true, public_key_valid(&PublicKey([0b01_11_11_11; PUBLICKEYBYTES]))); // 127
-    assert_eq!(false, public_key_valid(&PublicKey([0b10_00_00_00; PUBLICKEYBYTES]))); // 128
-    assert_eq!(false, public_key_valid(&PublicKey([0b11_11_11_11; PUBLICKEYBYTES]))); // 255
-
-    fn pk_from_u8(num: u8) {
-        let pk = PublicKey([num; PUBLICKEYBYTES]);
-
-        if num < 128 {
-            assert_eq!(true, public_key_valid(&pk));
-        } else {
-            assert_eq!(false, public_key_valid(&pk));
-        }
-    }
-    quickcheck(pk_from_u8 as fn(u8));
-    pk_from_u8(128);
+    assert!(public_key_valid(&PublicKey([0; PUBLICKEYBYTES]))); // 0
+    assert!(public_key_valid(&PublicKey([0b01_11_11_11; PUBLICKEYBYTES]))); // 127
+    assert!(!public_key_valid(&PublicKey([0b10_00_00_00; PUBLICKEYBYTES]))); // 128
+    assert!(!public_key_valid(&PublicKey([0b11_11_11_11; PUBLICKEYBYTES]))); // 255
 }
 
 
@@ -123,24 +103,20 @@ fn public_key_valid_test() {
 // test uses "bare" functions provided by `sodiumoxide`, with an exception
 // of the tested function
 fn encrypt_precompute_test() {
-    fn encrypt_decrypt_msg(msg: String) {
-        let (alice_pk, alice_sk) = gen_keypair();
-        let (bob_pk, bob_sk) = gen_keypair();
+    let (alice_pk, alice_sk) = gen_keypair();
+    let (bob_pk, bob_sk) = gen_keypair();
 
-        let alice_plaintext = &msg.into_bytes()[..];
-        let alice_precomputed_key = encrypt_precompute(&bob_pk, &alice_sk);
+    let alice_plaintext = b"Hi, Bob.";
+    let alice_precomputed_key = encrypt_precompute(&bob_pk, &alice_sk);
 
-        let nonce = gen_nonce();
+    let nonce = gen_nonce();
 
-        let ciphertext = seal_precomputed(alice_plaintext, &nonce, &alice_precomputed_key);
+    let ciphertext = seal_precomputed(alice_plaintext, &nonce, &alice_precomputed_key);
 
-        let bob_precomputed_key = encrypt_precompute(&alice_pk, &bob_sk);
-        let bob_plaintext = open_precomputed(&ciphertext, &nonce, &bob_precomputed_key).unwrap();
+    let bob_precomputed_key = encrypt_precompute(&alice_pk, &bob_sk);
+    let bob_plaintext = open_precomputed(&ciphertext, &nonce, &bob_precomputed_key).unwrap();
 
-        assert!(alice_plaintext == &bob_plaintext[..]);
-    }
-    encrypt_decrypt_msg(String::from_str("Hi, Bob.").unwrap());
-    quickcheck(encrypt_decrypt_msg as fn(String));
+    assert_eq!(alice_plaintext, &bob_plaintext[..]);
 }
 
 
@@ -148,23 +124,19 @@ fn encrypt_precompute_test() {
 // test uses "bare" functions provided by `sodiumoxide`, with an "exception"
 // of the tested function
 pub fn encrypt_data_symmetric_test() {
-    fn encrypt_decrypt_msg(msg: String) {
-        let (alice_pk, alice_sk) = gen_keypair();
-        let (bob_pk, bob_sk) = gen_keypair();
+    let (alice_pk, alice_sk) = gen_keypair();
+    let (bob_pk, bob_sk) = gen_keypair();
 
-        let alice_plain = &msg.into_bytes()[..];
+    let alice_plain = b"Hi, Bob.";
 
-        let precomputed_key = precompute(&bob_pk, &alice_sk);
-        let nonce = gen_nonce();
+    let precomputed_key = precompute(&bob_pk, &alice_sk);
+    let nonce = gen_nonce();
 
-        let ciphertext = encrypt_data_symmetric(&precomputed_key, &nonce, alice_plain);
+    let ciphertext = encrypt_data_symmetric(&precomputed_key, &nonce, alice_plain);
 
-        let bob_plain = open(&ciphertext, &nonce, &alice_pk, &bob_sk).unwrap();
+    let bob_plain = open(&ciphertext, &nonce, &alice_pk, &bob_sk).unwrap();
 
-        assert!(alice_plain == &bob_plain[..]);
-    }
-    encrypt_decrypt_msg(String::from_str("Hi, Bob.").unwrap());
-    quickcheck(encrypt_decrypt_msg as fn(String));
+    assert_eq!(alice_plain, &bob_plain[..]);
 }
 
 /* TODO: test for pubkey/skey/nonce being all `0`s, which would produce
@@ -182,23 +154,19 @@ pub fn encrypt_data_symmetric_test() {
 // test uses "bare" functions provided by `sodiumoxide`, with an exception
 // of the tested function
 fn decrypt_data_symmetric_test() {
-    fn encrypt_decrypt_msg(msg: String) {
-        let (alice_pk, alice_sk) = gen_keypair();
-        let (bob_pk, bob_sk) = gen_keypair();
+    let (alice_pk, alice_sk) = gen_keypair();
+    let (bob_pk, bob_sk) = gen_keypair();
 
-        let alice_plain = &msg.into_bytes()[..];
+    let alice_plain = b"Hi, Bob.";
 
-        let precomputed_key = precompute(&alice_pk, &bob_sk);
-        let nonce = gen_nonce();
+    let precomputed_key = precompute(&alice_pk, &bob_sk);
+    let nonce = gen_nonce();
 
-        let ciphertext = seal(alice_plain, &nonce, &bob_pk, &alice_sk);
+    let ciphertext = seal(alice_plain, &nonce, &bob_pk, &alice_sk);
 
-        let bob_plain = decrypt_data_symmetric(&precomputed_key, &nonce, &ciphertext).unwrap();
+    let bob_plain = decrypt_data_symmetric(&precomputed_key, &nonce, &ciphertext).unwrap();
 
-        assert!(alice_plain == &bob_plain[..]);
-    }
-    encrypt_decrypt_msg(String::from_str("Hi, Bob.").unwrap());
-    quickcheck(encrypt_decrypt_msg as fn(String));
+    assert_eq!(alice_plain, &bob_plain[..]);
 }
 
 
@@ -289,19 +257,10 @@ fn increment_nonce_number_test_0xff0000_plus_0x011000() {
 
 #[test]
 fn public_key_parse_bytes_test() {
-    fn with_bytes(bytes: Vec<u8>) {
-        if bytes.len() < PUBLICKEYBYTES {
-            assert!(PublicKey::from_bytes(&bytes).is_incomplete());
-            return
-        }
+    let bytes = [42; PUBLICKEYBYTES];
+    let (_rest, PublicKey(pk_bytes)) = PublicKey::from_bytes(&bytes).unwrap();
 
-        let (rest, PublicKey(pk_bytes)) = PublicKey::from_bytes(&bytes).unwrap();
-
-        assert_eq!(pk_bytes, &bytes[..PUBLICKEYBYTES]);
-        assert_eq!(rest, &bytes[PUBLICKEYBYTES..]);
-    }
-
-    quickcheck(with_bytes as fn(Vec<u8>));
+    assert_eq!(pk_bytes, &bytes as &[u8]);
 }
 
 
@@ -309,36 +268,18 @@ fn public_key_parse_bytes_test() {
 
 #[test]
 fn secret_key_parse_bytes_test() {
-    fn with_bytes(bytes: Vec<u8>) {
-        if bytes.len() < SECRETKEYBYTES {
-            assert!(SecretKey::from_bytes(&bytes).is_incomplete());
-            return
-        }
+    let bytes = [42; SECRETKEYBYTES];
+    let (_rest, SecretKey(sk_bytes)) = SecretKey::from_bytes(&bytes).unwrap();
 
-        let (rest, SecretKey(sk_bytes)) = SecretKey::from_bytes(&bytes).unwrap();
-
-        assert_eq!(sk_bytes, &bytes[..SECRETKEYBYTES]);
-        assert_eq!(rest, &bytes[SECRETKEYBYTES..]);
-    }
-
-    quickcheck(with_bytes as fn(Vec<u8>));
+    assert_eq!(sk_bytes, &bytes as &[u8]);
 }
 
 // Nonce::parse_bytes
 
 #[test]
 fn nonce_parse_bytes_test() {
-    fn with_bytes(bytes: Vec<u8>) {
-        if bytes.len() < NONCEBYTES {
-            assert!(Nonce::from_bytes(&bytes).is_incomplete());
-            return
-        }
+    let bytes = [42; NONCEBYTES];
+    let (_rest, Nonce(nonce_bytes)) = Nonce::from_bytes(&bytes).unwrap();
 
-        let (rest, Nonce(sk_bytes)) = Nonce::from_bytes(&bytes).unwrap();
-
-        assert_eq!(sk_bytes, &bytes[..NONCEBYTES]);
-        assert_eq!(rest, &bytes[NONCEBYTES..]);
-    }
-
-    quickcheck(with_bytes as fn(Vec<u8>));
+    assert_eq!(nonce_bytes, &bytes as &[u8]);
 }
