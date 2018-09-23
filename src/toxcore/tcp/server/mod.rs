2c01369d58dc51d61093b87a7f3c979eacfc339b
Evgeny Kurnevsky <kurnevsky@gmail.com>
2018-09-23 12:24:58 +0300

    fix(tcp_server): merge ServerProcessor with ServerExt

--- src/toxcore/tcp/server/mod.rs
+++ src/toxcore/tcp/server/mod.rs
@@ -3,10 +3,8 @@
 
 mod client;
 mod server;
-mod processor;
 mod server_ext;
 
 pub use self::client::Client;
 pub use self::server::Server;
-pub use self::processor::ServerProcessor;
 pub use self::server_ext::ServerExt;
