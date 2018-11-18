899285091f0952216c3ac61320a98616b732a553
Сухарик <suhr@i2pmail.org>
2018-11-18 17:18:38 +0300

    style(examples): get rid of visual identation

--- examples/tcp_client.rs
+++ examples/tcp_client.rs
@@ -36,24 +36,30 @@ use std::{thread, time};
 fn create_client(rx: mpsc::Receiver<Packet>, tx: mpsc::Sender<Packet>) -> IoFuture<()> {
     // Use `gen_keypair` to generate random keys
     // Client constant keypair for examples/tests
-    let client_pk = PublicKey([252, 72, 40, 127, 213, 13, 0, 95,
-                              13, 230, 176, 49, 69, 252, 220, 132,
-                              48, 73, 227, 58, 218, 154, 215, 245,
-                              23, 189, 223, 216, 153, 237, 130, 88]);
-    let client_sk = SecretKey([157, 128, 29, 197, 1, 72, 47, 56,
-                              65, 81, 191, 67, 220, 225, 108, 193,
-                              46, 163, 145, 242, 139, 125, 159,
-                              137, 174, 14, 225, 7, 138, 120, 185, 153]);
+    let client_pk = PublicKey([
+        252, 72, 40, 127, 213, 13, 0, 95,
+        13, 230, 176, 49, 69, 252, 220, 132,
+        48, 73, 227, 58, 218, 154, 215, 245,
+        23, 189, 223, 216, 153, 237, 130, 88
+    ]);
+    let client_sk = SecretKey([
+        157, 128, 29, 197, 1, 72, 47, 56,
+        65, 81, 191, 67, 220, 225, 108, 193,
+        46, 163, 145, 242, 139, 125, 159,
+        137, 174, 14, 225, 7, 138, 120, 185, 153
+    ]);
 
     let (addr, server_pk) = match 1 {
         1 => {
             // local tcp relay server from example
             let addr = "0.0.0.0:12345".parse().unwrap();
             // Server constant PK for examples/tests
-            let server_pk = PublicKey([177, 185, 54, 250, 10, 168, 174,
-                                    148, 0, 93, 99, 13, 131, 131, 239,
-                                    193, 129, 141, 80, 158, 50, 133, 100,
-                                    182, 179, 183, 234, 116, 142, 102, 53, 38]);
+            let server_pk = PublicKey([
+                177, 185, 54, 250, 10, 168, 174,
+                148, 0, 93, 99, 13, 131, 131, 239,
+                193, 129, 141, 80, 158, 50, 133, 100,
+                182, 179, 183, 234, 116, 142, 102, 53, 38
+            ]);
             (addr, server_pk)
         },
         2 => {
@@ -136,10 +142,12 @@ fn create_client(rx: mpsc::Receiver<Packet>, tx: mpsc::Sender<Packet>) -> IoFutu
 #[allow(dead_code)]
 fn send_packets(tx: mpsc::Sender<Packet>) {
     // Client friend constant PK for examples/tests
-    let friend_pk = PublicKey([15, 107, 126, 130, 81, 55, 154, 157,
-                            192, 117, 0, 225, 119, 43, 48, 117,
-                            84, 109, 112, 57, 243, 216, 4, 171,
-                            185, 111, 33, 146, 221, 31, 77, 118]);
+    let friend_pk = PublicKey([
+        15, 107, 126, 130, 81, 55, 154, 157,
+        192, 117, 0, 225, 119, 43, 48, 117,
+        84, 109, 112, 57, 243, 216, 4, 171,
+        185, 111, 33, 146, 221, 31, 77, 118
+    ]);
 
     let mut i = 0u64;
     loop {
@@ -197,10 +205,12 @@ fn main() {
         }
         i = i + 1;
         // Client friend constant PK for examples/tests
-        let friend_pk = PublicKey([15, 107, 126, 130, 81, 55, 154, 157,
-                                192, 117, 0, 225, 119, 43, 48, 117,
-                                84, 109, 112, 57, 243, 216, 4, 171,
-                                185, 111, 33, 146, 221, 31, 77, 118]);
+        let friend_pk = PublicKey([
+            15, 107, 126, 130, 81, 55, 154, 157,
+            192, 117, 0, 225, 119, 43, 48, 117,
+            84, 109, 112, 57, 243, 216, 4, 171,
+            185, 111, 33, 146, 221, 31, 77, 11
+        ]);
 
         let request = if i == 1 {
             tx.send(Packet::RouteRequest(RouteRequest {pk: friend_pk } ))
