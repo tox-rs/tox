3f6465624c9d759e6c4da7f4eb43c237a12cdc1a
Evgeny Kurnevsky <kurnevsky@gmail.com>
2019-04-14 18:53:49 +0300

    refactor(net_crypto): use helper macro for HandlePacketError

--- src/toxcore/net_crypto/errors.rs
+++ src/toxcore/net_crypto/errors.rs
@@ -11,7 +11,6 @@ use tokio::timer::Error as TimerError;
 use tokio::timer::timeout::Error as TimeoutError;
 
 use crate::toxcore::dht::packet::*;
-use crate::toxcore::crypto_core::*;
 
 error_kind! {
     #[doc = "Error that can happen while processing packets array"]
@@ -68,18 +67,79 @@ impl PacketsArrayError {
     }
 }
 
-/// Error that can happen when calling `handle_*` of packet.
-#[derive(Debug)]
-pub struct HandlePacketError {
-    ctx: Context<HandlePacketErrorKind>,
+error_kind! {
+    #[doc = "Error that can happen when calling `handle_*` of packet."]
+    #[derive(Debug)]
+    HandlePacketError,
+    #[doc = "The specific kind of error that can occur."]
+    #[derive(Debug, Eq, PartialEq, Fail)]
+    HandlePacketErrorKind {
+        #[doc = "Error indicates that getting payload of received packet error."]
+        #[fail(display = "Get payload of received packet error")]
+        GetPayload,
+        #[doc = "Error indicates that sending response packet error."]
+        #[fail(display = "Sending response error")]
+        SendTo,
+        #[doc = "Error indicates that sending lossless response packet error."]
+        #[fail(display = "Sending lossless response error")]
+        SendToLossless,
+        #[doc = "Error indicates that sending lossy response packet error."]
+        #[fail(display = "Sending lossy response error")]
+        SendToLossy,
+        #[doc = "Error indicates that sending dhtpk packet error."]
+        #[fail(display = "Sending dhtpk packet error")]
+        SendToDhtpk,
+        #[doc = "Error indicates that NetCrypto can't handle packet in current connection state."]
+        #[fail(display = "Can't handle CookieResponse in current connection state")]
+        InvalidState,
+        #[doc = "Error indicates that NetCrypto can't handle crypto data packet in current connection state."]
+        #[fail(display = "Can't handle CryptoData in current connection state")]
+        CannotHandleCryptoData,
+        #[doc = "Error indicates that invalid cookie request id."]
+        #[fail(display = "Invalid cookie request id: expected {:?} but got {:?}", expect, got)]
+        InvalidRequestId {
+            #[doc = "Expected request id."]
+            expect: u64,
+            #[doc = "Gotten request id."]
+            got: u64,
+        },
+        #[doc = "Error indicates that no crypto connection for address."]
+        #[fail(display = "No crypto connection for address: {:?}", addr)]
+        NoConnection {
+            #[doc = "The address for connection which don't exist."]
+            addr: SocketAddr,
+        },
+        #[doc = "Unexpected crypto handshake."]
+        #[fail(display = "Unexpected crypto handshake")]
+        UnexpectedCryptoHandshake,
+        #[doc = "Error indicates that invalid SHA512 hash of cookie."]
+        #[fail(display = "Invalid SHA512 hash of cookie")]
+        BadSha512,
+        #[doc = "Error indicates that cookie is timed out."]
+        #[fail(display = "Cookie is timed out")]
+        CookieTimedOut,
+        #[doc = "Error indicates that cookie contains invalid real pk."]
+        #[fail(display = "Cookie contains invalid real pk")]
+        InvalidRealPk,
+        #[doc = "Error indicates that cookie contains invalid dht pk."]
+        #[fail(display = "Cookie contains invalid dht pk")]
+        InvalidDhtPk,
+        #[doc = "Error indicates that there is PacketsArrayError."]
+        #[fail(display = "PacketsArrayError occurs")]
+        PacketsArrayError,
+        #[doc = "Error indicates that real data is empty."]
+        #[fail(display = "Real data is empty")]
+        DataEmpty,
+        #[doc = "Error indicates that invalid packet id."]
+        #[fail(display = "Invalid packet id: {:?}", id)]
+        PacketId {
+            #[doc = "The packet id that is invalid."]
+            id: u8,
+        },
+    }
 }
 
 impl HandlePacketError {
-    /// Return the kind of this error.
-    pub fn kind(&self) -> &HandlePacketErrorKind {
-        self.ctx.get_context()
-    }
-
     pub(crate) fn invalid_request_id(expect: u64, got: u64) -> HandlePacketError {
         HandlePacketError::from(HandlePacketErrorKind::InvalidRequestId {
             expect,
@@ -97,138 +157,6 @@ impl HandlePacketError {
         HandlePacketError::from(HandlePacketErrorKind::PacketId {
             id,
         })
-    }
-}
-
-impl Fail for HandlePacketError {
-    fn cause(&self) -> Option<&Fail> {
-        self.ctx.cause()
-    }
-
-    fn backtrace(&self) -> Option<&Backtrace> {
-        self.ctx.backtrace()
-    }
-}
-
-impl fmt::Display for HandlePacketError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        self.ctx.fmt(f)
-    }
-}
-
-/// The specific kind of error that can occur.
-#[derive(Debug, Eq, PartialEq, Fail)]
-pub enum HandlePacketErrorKind {
-    /// Error indicates that getting payload of received packet error.
-    #[fail(display = "Get payload of received packet error")]
-    GetPayload,
-    /// Error indicates that sending response packet error.
-    #[fail(display = "Sending response error")]
-    SendTo,
-    /// Error indicates that sending lossless response packet error.
-    #[fail(display = "Sending lossless response error")]
-    SendToLossless,
-    /// Error indicates that sending dhtpk packet error.
-    #[fail(display = "Sending dhtpk packet error")]
-    SendToDhtpk,
-    /// Error indicates that NetCrypto can't handle packet in current connection state.
-    #[fail(display = "Can't handle CookieResponse in current connection state")]
-    InvalidState,
-    /// Error indicates that NetCrypto can't handle crypto data packet in current connection state.
-    #[fail(display = "Can't handle CryptoData in current connection state")]
-    CannotHandleCryptoData,
-    /// Error indicates that invalid cookie request id.
-    #[fail(display = "Invalid cookie request id: expected {:?} but got {:?}", expect, got)]
-    InvalidRequestId {
-        /// Expected request id
-        expect: u64,
-        /// Gotten request id
-        got: u64,
-    },
-    /// Error indicates that no crypto connection for address.
-    #[fail(display = "No crypto connection for address: {:?}", addr)]
-    NoConnection {
-        /// The address for connection which don't exist
-        addr: SocketAddr,
-    },
-    /// Unexpected crypto handshake.
-    #[fail(display = "Unexpected crypto handshake")]
-    UnexpectedCryptoHandshake,
-    /// Error indicates that invalid SHA512 hash of cookie
-    #[fail(display = "Invalid SHA512 hash of cookie")]
-    BadSha512,
-    /// Error indicates that cookie is timed out
-    #[fail(display = "Cookie is timed out")]
-    CookieTimedOut,
-    /// Error indicates that cookie contains invalid real pk
-    #[fail(display = "Cookie contains invalid real pk")]
-    InvalidRealPk,
-    /// Error indicates that cookie contains invalid dht pk
-    #[fail(display = "Cookie contains invalid dht pk")]
-    InvalidDhtPk,
-    /// Error indicates that there is PacketsArrayError
-    #[fail(display = "PacketsArrayError occurs")]
-    PacketsArrayError,
-    /// Error indicates that real data is empty
-    #[fail(display = "Real data is empty")]
-    DataEmpty,
-    /// Error indicates that invalid packet id.
-    #[fail(display = "Invalid packet id: {:?}", id)]
-    PacketId {
-        /// The packet id that is invalid
-        id: u8,
-    },
-}
-
-impl From<HandlePacketErrorKind> for HandlePacketError {
-    fn from(kind: HandlePacketErrorKind) -> HandlePacketError {
-        HandlePacketError::from(Context::new(kind))
-    }
-}
-
-impl From<TimeoutError<mpsc::SendError<(Packet, SocketAddr)>>> for HandlePacketError {
-    fn from(error: TimeoutError<mpsc::SendError<(Packet, SocketAddr)>>) -> HandlePacketError {
-        HandlePacketError {
-            ctx: error.context(HandlePacketErrorKind::SendTo)
-        }
-    }
-}
-
-impl From<mpsc::SendError<(PublicKey, Vec<u8>)>> for HandlePacketError {
-    fn from(error: mpsc::SendError<(PublicKey, Vec<u8>)>) -> HandlePacketError {
-        HandlePacketError {
-            ctx: error.context(HandlePacketErrorKind::SendToLossless)
-        }
-    }
-}
-
-impl From<mpsc::SendError<(PublicKey, PublicKey)>> for HandlePacketError {
-    fn from(error: mpsc::SendError<(PublicKey, PublicKey)>) -> HandlePacketError {
-        HandlePacketError {
-            ctx: error.context(HandlePacketErrorKind::SendToDhtpk)
-        }
-    }
-}
-
-impl From<Context<HandlePacketErrorKind>> for HandlePacketError {
-    fn from(ctx: Context<HandlePacketErrorKind>) -> HandlePacketError {
-        HandlePacketError { ctx }
-    }
-}
-
-impl From<GetPayloadError> for HandlePacketError {
-    fn from(error: GetPayloadError) -> HandlePacketError {
-        HandlePacketError {
-            ctx: error.context(HandlePacketErrorKind::GetPayload)
-        }
-    }
-}
-
-impl From<PacketsArrayError> for HandlePacketError {
-    fn from(error: PacketsArrayError) -> HandlePacketError {
-        HandlePacketError {
-            ctx: error.context(HandlePacketErrorKind::PacketsArrayError)
-        }
     }
 }
 
@@ -478,158 +406,6 @@ mod tests {
 
     use std::time::Duration;
     use futures::future::Future;
-
-    #[test]
-    fn handle_packet_error() {
-        let error = HandlePacketError::from(HandlePacketErrorKind::InvalidState);
-        assert!(error.cause().is_none());
-    }
-
-    #[test]
-    fn handle_packet_invalid_request_id() {
-        let error = HandlePacketError::invalid_request_id(1, 0);
-        assert_eq!(*error.kind(), HandlePacketErrorKind::InvalidRequestId { expect: 1, got: 0 });
-        assert_eq!(format!("{}", error), "Invalid cookie request id: expected 1 but got 0".to_owned());
-    }
-
-    #[test]
-    fn handle_packet_no_connection() {
-        let sock = "127.0.0.1:33445".parse().unwrap();
-        let error = HandlePacketError::no_connection(sock);
-        assert_eq!(*error.kind(), HandlePacketErrorKind::NoConnection { addr: sock });
-        assert_eq!(format!("{}", error), "No crypto connection for address: V4(127.0.0.1:33445)".to_owned());
-    }
-
-    #[test]
-    fn handle_packet_unexpected_crypto_handshake() {
-        let error = HandlePacketError::from(HandlePacketErrorKind::UnexpectedCryptoHandshake);
-        assert_eq!(*error.kind(), HandlePacketErrorKind::UnexpectedCryptoHandshake);
-        assert_eq!(format!("{}", error), "Unexpected crypto handshake".to_owned());
-    }
-
-    #[test]
-    fn handle_packet_packet_id() {
-        let error = HandlePacketError::packet_id(1);
-        assert_eq!(*error.kind(), HandlePacketErrorKind::PacketId { id: 1 });
-        assert_eq!(format!("{}", error), "Invalid packet id: 1".to_owned());
-    }
-
-    #[test]
-    fn handle_packet_get_payload() {
-        let error = HandlePacketError::from(HandlePacketErrorKind::GetPayload);
-        assert_eq!(*error.kind(), HandlePacketErrorKind::GetPayload);
-        assert_eq!(format!("{}", error), "Get payload of received packet error".to_owned());
-
-        let get_payload_error = GetPayloadError::from(GetPayloadErrorKind::Decrypt);
-        let error = HandlePacketError::from(get_payload_error);
-        assert_eq!(*error.kind(), HandlePacketErrorKind::GetPayload);
-        assert_eq!(format!("{}", error), "Get payload of received packet error".to_owned());
-    }
-
-    #[test]
-    fn handle_packet_send_to() {
-        let (tx, rx) = mpsc::channel(32);
-
-        let packet = Packet::BootstrapInfo(BootstrapInfo {
-            version: 1717,
-            motd: vec![1, 2, 3, 4],
-        });
-        let sock: SocketAddr = "127.0.0.1:33445".parse().unwrap();
-
-        drop(rx);
-        let res = send_to_bounded(&tx, (packet, sock), Duration::from_secs(1)).wait();
-        assert!(res.is_err());
-        let error = HandlePacketError::from(res.err().unwrap());
-        assert_eq!(*error.kind(), HandlePacketErrorKind::SendTo);
-        assert_eq!(format!("{}", error), "Sending response error".to_owned());
-    }
-
-    #[test]
-    fn handle_packet_send_to_lossless() {
-        let (tx, rx) = mpsc::channel(32);
-
-        let pk = gen_keypair().0;
-        let data = vec![1, 2, 3];
-
-        drop(rx);
-        let res = send_to(&tx, (pk, data)).wait();
-        assert!(res.is_err());
-        let error = HandlePacketError::from(res.err().unwrap());
-        assert_eq!(*error.kind(), HandlePacketErrorKind::SendToLossless);
-        assert_eq!(format!("{}", error), "Sending lossless response error".to_owned());
-    }
-
-    #[test]
-    fn handle_packet_send_to_dhtpk() {
-        let (tx, rx) = mpsc::channel(32);
-
-        let pk = gen_keypair().0;
-        let dhtpk = gen_keypair().0;
-
-        drop(rx);
-        let res = send_to(&tx, (pk, dhtpk)).wait();
-        assert!(res.is_err());
-        let error = HandlePacketError::from(res.err().unwrap());
-        assert_eq!(*error.kind(), HandlePacketErrorKind::SendToDhtpk);
-        assert_eq!(format!("{}", error), "Sending dhtpk packet error".to_owned());
-    }
-
-    #[test]
-    fn handle_packet_invalid_state() {
-        let error = HandlePacketError::from(HandlePacketErrorKind::InvalidState);
-        assert_eq!(*error.kind(), HandlePacketErrorKind::InvalidState);
-        assert_eq!(format!("{}", error), "Can't handle CookieResponse in current connection state".to_owned());
-    }
-
-    #[test]
-    fn handle_packet_cannot_handle_crypto_data() {
-        let error = HandlePacketError::from(HandlePacketErrorKind::CannotHandleCryptoData);
-        assert_eq!(*error.kind(), HandlePacketErrorKind::CannotHandleCryptoData);
-        assert_eq!(format!("{}", error), "Can't handle CryptoData in current connection state".to_owned());
-    }
-
-    #[test]
-    fn handle_packet_bad_sha512() {
-        let error = HandlePacketError::from(HandlePacketErrorKind::BadSha512);
-        assert_eq!(*error.kind(), HandlePacketErrorKind::BadSha512);
-        assert_eq!(format!("{}", error), "Invalid SHA512 hash of cookie".to_owned());
-    }
-
-    #[test]
-    fn handle_packet_cookie_timedout() {
-        let error = HandlePacketError::from(HandlePacketErrorKind::CookieTimedOut);
-        assert_eq!(*error.kind(), HandlePacketErrorKind::CookieTimedOut);
-        assert_eq!(format!("{}", error), "Cookie is timed out".to_owned());
-    }
-
-    #[test]
-    fn handle_packet_invalid_real_pk() {
-        let error = HandlePacketError::from(HandlePacketErrorKind::InvalidRealPk);
-        assert_eq!(*error.kind(), HandlePacketErrorKind::InvalidRealPk);
-        assert_eq!(format!("{}", error), "Cookie contains invalid real pk".to_owned());
-    }
-
-    #[test]
-    fn handle_packet_invalid_dht_pk() {
-        let error = HandlePacketError::from(HandlePacketErrorKind::InvalidDhtPk);
-        assert_eq!(*error.kind(), HandlePacketErrorKind::InvalidDhtPk);
-        assert_eq!(format!("{}", error), "Cookie contains invalid dht pk".to_owned());
-    }
-
-    #[test]
-    fn handle_packet_packets_array_error() {
-        let error = PacketsArrayError::from(PacketsArrayErrorKind::ArrayFull);
-        let error = HandlePacketError::from(error);
-        assert_eq!(*error.kind(), HandlePacketErrorKind::PacketsArrayError);
-        assert_eq!(format!("{}", error), "PacketsArrayError occurs".to_owned());
-    }
-
-    #[test]
-    fn handle_packet_data_empty() {
-        let error = HandlePacketError::from(HandlePacketErrorKind::DataEmpty);
-        assert_eq!(*error.kind(), HandlePacketErrorKind::DataEmpty);
-        assert_eq!(format!("{}", error), "Real data is empty".to_owned());
-    }
 
     #[test]
     fn send_data_error() {
