101a847e0ecd2c40c0afbe8eb401fe6bdde9264d
Evgeny Kurnevsky <kurnevsky@gmail.com>
2019-04-15 16:31:23 +0300

    refactor(daemon_state): use helper macro for DeserializeError

--- src/toxcore/dht/daemon_state.rs
+++ src/toxcore/dht/daemon_state.rs
@@ -14,89 +14,42 @@ use crate::toxcore::binary_io::*;
 use crate::toxcore::dht::kbucket::*;
 use crate::toxcore::dht::ktree::*;
 
-use std::fmt;
-
-use failure::{Backtrace, Context, Fail};
+use failure::Fail;
 use nom::{Needed, ErrorKind as NomErrorKind};
 
-/// An error that can occur while serializing/deserializing object
-#[derive(Debug)]
-pub struct DeserializeError {
-    ctx: Context<DeserializeErrorKind>,
+error_kind! {
+    #[doc = "An error that can occur while serializing/deserializing object."]
+    #[derive(Debug)]
+    DeserializeError,
+    #[doc = "The specific kind of error that can occur."]
+    #[derive(Clone, Debug, Eq, PartialEq, Fail)]
+    DeserializeErrorKind {
+        #[doc = "Error indicates that object can't be parsed."]
+        #[fail(display = "Deserialize object error: {:?}, data: {:?}", error, data)]
+        Deserialize {
+            #[doc = "Parsing error."]
+            error: NomErrorKind,
+            #[doc = "Object serialized data."]
+            data: Vec<u8>,
+        },
+        #[doc = "Error indicates that more data is needed to parse serialized object."]
+        #[fail(display = "Bytes of object should not be incomplete: {:?}, data: {:?}", needed, data)]
+        IncompleteData {
+            #[doc = "Required data size to be parsed."]
+            needed: Needed,
+            #[doc = "Object serialized data."]
+            data: Vec<u8>,
+        },
+    }
 }
 
 impl DeserializeError {
-    /// Return the kind of this error.
-    pub fn kind(&self) -> &DeserializeErrorKind {
-        self.ctx.get_context()
-    }
-
     pub(crate) fn incomplete(needed: Needed, data: Vec<u8>) -> DeserializeError {
         DeserializeError::from(DeserializeErrorKind::IncompleteData { needed, data })
     }
 
     pub(crate) fn deserialize(error: NomErrorKind, data: Vec<u8>) -> DeserializeError {
         DeserializeError::from(DeserializeErrorKind::Deserialize { error, data })
-    }
-}
-
-impl Fail for DeserializeError {
-    fn cause(&self) -> Option<&Fail> {
-        self.ctx.cause()
-    }
-
-    fn backtrace(&self) -> Option<&Backtrace> {
-        self.ctx.backtrace()
-    }
-}
-
-impl fmt::Display for DeserializeError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        self.ctx.fmt(f)
-    }
-}
-
-/// The specific kind of error that can occur.
-#[derive(Clone, Debug, Eq, PartialEq)]
-pub enum DeserializeErrorKind {
-    /// Error indicates that object can't be parsed.
-    Deserialize {
-        /// Parsing error
-        error: NomErrorKind,
-        /// Object serialized data
-        data: Vec<u8>,
-    },
-    /// Error indicates that more data is needed to parse serialized object.
-    IncompleteData {
-        /// Required data size to be parsed
-        needed: Needed,
-        /// Object serialized data
-        data: Vec<u8>,
-    },
-}
-
-impl fmt::Display for DeserializeErrorKind {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match *self {
-            DeserializeErrorKind::Deserialize { ref error, ref data } => {
-                write!(f, "Deserialize object error: {:?}, data: {:?}", error, data)
-            },
-            DeserializeErrorKind::IncompleteData { ref needed, ref data } => {
-                write!(f, "Bytes of object should not be incomplete: {:?}, data: {:?}", needed, data)
-            },
-        }
-    }
-}
-
-impl From<DeserializeErrorKind> for DeserializeError {
-    fn from(kind: DeserializeErrorKind) -> DeserializeError {
-        DeserializeError::from(Context::new(kind))
-    }
-}
-
-impl From<Context<DeserializeErrorKind>> for DeserializeError {
-    fn from(ctx: Context<DeserializeErrorKind>) -> DeserializeError {
-        DeserializeError { ctx }
     }
 }
 
@@ -206,21 +159,5 @@ mod tests {
         alice.close_nodes.write().remove(&pk_org);
         let serialized_vec = DaemonState::serialize_old(&alice);
         assert!(DaemonState::deserialize_old(&alice, &serialized_vec).wait().is_ok());
-    }
-
-    #[test]
-    fn ser_de_error() {
-        let error = DeserializeError::deserialize(NomErrorKind::Eof, vec![1,2,3,4]);
-        assert!(error.cause().is_none());
-        assert!(error.backtrace().is_some());
-        assert_eq!(format!("{}", error), "Deserialize object error: Eof, data: [1, 2, 3, 4]".to_owned());
-    }
-
-    #[test]
-    fn ser_de_error_kind() {
-        let incomplete = DeserializeErrorKind::IncompleteData { needed: Needed::Size(5), data: vec![1,2,3,4] };
-        assert_eq!(format!("{}", incomplete), "Bytes of object should not be incomplete: Size(5), data: [1, 2, 3, 4]".to_owned());
-        let deserialize = DeserializeErrorKind::Deserialize { error: NomErrorKind::Eof, data: vec![1,2,3,4] };
-        assert_eq!(format!("{}", deserialize), "Deserialize object error: Eof, data: [1, 2, 3, 4]".to_owned());
     }
 }
