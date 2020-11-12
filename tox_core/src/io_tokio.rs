3545bb3f8986755a7b4ac1bd66919285043b7a29
Evgeny Kurnevsky <kurnevsky@gmail.com>
2020-11-12 20:11:37 +0300

    feat(future): remove unused type definitions

--- tox_core/src/io_tokio.rs
+++ tox_core/src/io_tokio.rs
@@ -1,15 +1,6 @@
 //! Defines `IoFuture` and `IoStream`
 
-use std::io::{Error as IoError};
-
-use futures::{Future, Stream};
 use futures::sink::SinkExt;
-
-/// A convenience typedef around a `Future` whose error component is `io::Error`
-pub type IoFuture<T> = Box<dyn Future<Output = Result<T, IoError>> + Send>;
-
-/// A convenience typedef around a `Stream` whose error component is `io::Error`
-pub type IoStream<T> = Box<dyn Stream<Item = Result<T, IoError>> + Send>;
 
 /// Sends a message into `Option<Sender<T>>`
 pub async fn maybe_send_bounded<T>(
