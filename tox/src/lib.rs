c697b554d8d874418bee7e560fdc33edee969aa5
Nils Fenner <nilsfenner@web.de>
2023-11-21 13:05:02 +0100

    Add functions for tox crate version information

--- tox/src/lib.rs
+++ tox/src/lib.rs
@@ -4,3 +4,16 @@ pub use tox_core as core;
 pub use tox_crypto as crypto;
 pub use tox_encryptsave as encryptsave;
 pub use tox_packet as packet;
+
+pub fn crate_version() -> String {
+    env!("CARGO_PKG_VERSION").to_string()
+}
+pub fn crate_version_major() -> u32 {
+    env!("CARGO_PKG_VERSION_MAJOR").parse().expect("Invalid major version")
+}
+pub fn crate_version_minor() -> u32 {
+    env!("CARGO_PKG_VERSION_MINOR").parse().expect("Invalid minor version")
+}
+pub fn crate_version_patch() -> u32 {
+    env!("CARGO_PKG_VERSION_PATCH").parse().expect("Invalid patch version")
+}
