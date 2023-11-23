4d4ecdd603241d1f36ae640697c1e0bae4e81160
Nils Fenner <nilsfenner@web.de>
2023-11-23 10:36:04 +0100

    Add crate version tests

--- tox/src/lib.rs
+++ tox/src/lib.rs
@@ -17,3 +17,26 @@ pub fn crate_version_minor() -> u32 {
 pub fn crate_version_patch() -> u32 {
     env!("CARGO_PKG_VERSION_PATCH").parse().expect("Invalid patch version")
 }
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn crate_version_is_not_empty() {
+        assert_ne!(crate::crate_version(), "");
+    }
+
+    #[test]
+    fn crate_version_major() {
+        crate::crate_version_major();
+    }
+
+    #[test]
+    fn crate_version_minor() {
+        crate::crate_version_minor();
+    }
+
+    #[test]
+    fn crate_version_patch() {
+        crate::crate_version_patch();
+    }
+}
